1c9488ce2374a7d2f852460d16323fb5368ad067
Kaung Khant Thar <kaungkhantthar@ucsm.edu.mm>
2021-01-04 16:20:59 +0630

    Added basic grahpql api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-// index.js
-// This is the main entry point of our application
-
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world!!!'));
+const notes = [
+  { id: '1', content: 'Learn Graphql', author: 'Kaung Khant Thar' },
+  { id: '2', content: 'Learn Nextjs and Graphql', author: 'Sayargyi' },
+  { id: '3', content: 'Learn Python', author: 'KKT' }
+];
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello world graphql',
+    notes: () => notes,
+    note: (parent, args) => {
+      const note = notes.find(note => note.id === args.id);
+
+      return note;
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const newNote = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Kaung Khant Thar'
+      };
+      notes.push(newNote);
+      return newNote;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`server is listening on port ${port}`));
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
